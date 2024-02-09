d2b0af874ed145ed04a95d17724afe3fb3d1fd9f
Vishal Ranka <vishalranka3@gmail.com>
2024-02-10 01:15:46 +0530

    render_configs_done

--- Backend/server.js
+++ Backend/server.js
@@ -26,11 +26,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-app.get;
-"*",
-  function (req, res) {
-    res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
-  };
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
+});
 app.listen(PORT, () => {
   console.log("Your server is started at http://localhost:" + PORT);
 });
